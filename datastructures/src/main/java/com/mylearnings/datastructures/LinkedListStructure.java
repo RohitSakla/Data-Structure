1d9849d270ef9672356dba12a851becec93799c1
RohitSakla <saklarohit@hotmail.com>
2017-01-18 23:56:37 +0530

    Simple LnkedList Operations

--- datastructures/src/main/java/com/mylearnings/datastructures/LinkedListStructure.java
+++ datastructures/src/main/java/com/mylearnings/datastructures/LinkedListStructure.java
@@ -1,5 +1,42 @@
 package com.mylearnings.datastructures;
 
 public class LinkedListStructure {
+	
+}
 
+class LinkedList {
+	Node head;
+	
+	public void push(Node node) {
+		node.next = head;
+		head = node;
+	}
+	
+	public void insertAfter(Node previousNode,Node node){
+		node.next = previousNode.next;
+		previousNode.next = node;
+	}
+	
+	public void append(Node node) {
+		if(head == null) {
+			head.next = node;
+			return;
+		}
+		
+		Node next = head;
+		while(next.next != null) {
+			next = next.next;
+		}
+		
+		next.next = node;
+		return;
+	}
+	class Node {
+		int data;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+		}
+	}
 }
